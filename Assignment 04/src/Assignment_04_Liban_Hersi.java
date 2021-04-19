8fa8aa26fa5e7472c0aab981a2941b56dc90eea5
RBTG <RBTG@Noman>
2021-04-19 05:57:24 +0500

    Corrected the problem with the program output.if values[index] % 2 == 0

--- Assignment 04/src/Assignment_04_Liban_Hersi.java
+++ Assignment 04/src/Assignment_04_Liban_Hersi.java
@@ -4,8 +4,8 @@ public class Assignment_04_Liban_Hersi
 	{
 		int[] values = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		System.out.print("Even numbers are: ");
-		for(int index = 0; index < values.length - 1; index++) {
-			    if(values[index] % 2 == 1){         
+		for(int index = 0; index < values.length; index++) {
+			    if(values[index] % 2 == 0){         
 			        System.out.print(values[index] + " ");
 			    }
 		}
