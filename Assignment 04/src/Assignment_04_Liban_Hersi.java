19a0dbdc9f4293ade4ea834b8785cc796377a59c
RBTG <RBTG@Noman>
2021-04-19 05:49:28 +0500

    Added programmer comments, and starter code to method main.

--- Assignment 04/src/Assignment_04_Liban_Hersi.java
+++ Assignment 04/src/Assignment_04_Liban_Hersi.java
@@ -1,19 +1,15 @@
-/**
- * 
- */
-
-/**
- * @author RBTG
- *
- */
-public class Assignment_04_Liban_Hersi {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Assignment_04_Liban_Hersi 
+{
+	public static void main(String[] args) 
+	{
+		int[] values = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		System.out.print("Even numbers are: ");
+		for(int index = 0; index < values.length - 1; index++) {
+			    if(values[index] % 2 == 1){         
+			        System.out.print(values[index] + " ");
+			    }
+		}
+		System.out.println(); // newline after loop
+		System.out.println("Program by Liban Hersi Here");
 	}
-
 }
